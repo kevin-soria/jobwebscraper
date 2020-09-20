20b9a6bf27ab9df6c11287b777a801621f7a6705
Kevin Soria <Kevin.Soria@outlook.com>
2020-09-19 23:11:46 -0400

    fifth

--- wepscraper.py
+++ wepscraper.py
@@ -13,7 +13,15 @@ html_doc = """<!DOCTYPE html>
         <!--[if lt IE 7]>
             <p class=h"browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-        <h1>Hello World</h1>
+        <div id="section-1">
+            <h1>Hello World</h1>
+
+        </div>
+        <div id="section-2">
+            <h3> Hello</h3>
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F677439968930881544%2F&psig=AOvVaw0OjtDrXJDGKORJ5uYfm7zl&ust=1600657748268000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIim9PTg9usCFQAAAAAdAAAAABAI" />
+            <p> I am the P tag</p>
+        </div>
         <script src="" async defer></script>
     </body>
 </html>"""
